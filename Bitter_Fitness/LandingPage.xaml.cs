46f2f8695aeb2d12ef12489dae211506a48d20d3
MichaelT-Smith <michaels@fireflyint.com>
2023-07-10 13:51:04 -0400

    App breaking bug fix

---
+++ Bitter_Fitness/LandingPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Bitter_Fitness
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LandingPage : ContentPage
+    {
+        public LandingPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
