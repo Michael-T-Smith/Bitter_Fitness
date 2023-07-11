489d3ad0995a08954f40e0a7de67f9cb248438fc
Nick-Sciascia <nick.sciascia@gmail.com>
2023-07-10 20:54:02 -0700

    Installed SQLite & created Person +  Database classes

--- Bitter_Fitness/App.xaml.cs
+++ Bitter_Fitness/App.xaml.cs
@@ -1,14 +1,34 @@
 ﻿using Bitter_Fitness.Services;
 using Bitter_Fitness.Views;
 using System;
+using System.IO;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace Bitter_Fitness
 {
     public partial class App : Application
+
     {
 
+        private static Database database;
+
+        /* Maintains one database connection.*/
+        /* Change Environment Special Folder path to desired location for storing database file(s).*/
+        /* Reference https://youtu.be/uxqQqyuZ3Qo?t=600 for more implementation details */
+        public static Database Database
+        {
+            get
+            {
+                if (database == null) 
+                {
+                    database = new Database(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "people.db3"));
+                }
+
+                return database;
+            }
+
+        }
         public App()
         {
             InitializeComponent();
